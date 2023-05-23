7324aa2ea03d067e130a99df96a56a4e5f599e40
amalia1718 <amally1818@gmail.com>
2023-05-22 23:28:43 -0700

    Created divided method

---
+++ src/Methods/divideMethod.java
@@ -0,0 +1,18 @@
+package Methods;
+
+public class divideMethod {
+    public static void main(String[] args) {
+
+
+        int x = 36;
+        int y = 6;
+        int count = 0;
+
+        while (x >= y) {
+            x = x - y;
+            count++;
+
+        }
+        System.out.println(count);
+    }
+    }
