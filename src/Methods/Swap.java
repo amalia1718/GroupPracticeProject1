5366cf77f45ec53adaf0c4043fcb1e3a7fdf8683
amalia1718 <amally1818@gmail.com>
2023-06-01 13:27:20 -0700

    Created swap method

---
+++ src/Methods/Swap.java
@@ -0,0 +1,22 @@
+package Methods;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        int a = 2;
+      int b = 7;
+
+
+
+
+    a = a + b;// adds the values of a and b and assigns the sum to a
+    b = a - b;// substract new value of b from the value new value of a
+    a = a - b;//substract the new value of b from the new value of b
+
+    System.out.println("a="+ a);
+    System.out.println("b="+b);
+
+
+}
+
+}
