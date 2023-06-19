626e458efacf896ad44169afb90d3ec5b533f4f0
amalia1718 <amally1818@gmail.com>
2023-06-19 15:34:08 -0700

    made some changes

--- src/Methods/Reverse.java
+++ src/Methods/Reverse.java
@@ -8,8 +8,8 @@ public class Reverse {
         String str = "ABCD";
         String reverse = "";
 
-        for (int i = str.length() - 1; i >= 0; i--) { // i: index number of the given string starting from last index to index 0
-            reverse += str.charAt(i);  // to get each character of the string starting from last index to index zero
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
         }
 
 
